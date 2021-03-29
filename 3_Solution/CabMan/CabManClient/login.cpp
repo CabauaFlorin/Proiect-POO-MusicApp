137a43f4db0f2ccb44b95021676a3485b8c29e1e
FLORIN-GABRIEL CABAUA <79976336+CabauaFlorin@users.noreply.github.com>
2021-03-29 22:42:29 +0300

    [UPDATE] Solution Scheme

---
+++ 3_Solution/CabMan/CabManClient/login.cpp
@@ -0,0 +1,8 @@
+#include "login.h"
+#include "stdafx.h"
+
+CabManClient::CabManClient(QWidget *parent)
+    : QWidget(parent)
+{
+    ui.setupUi(this);
+}
---
+++ 3_Solution/CabMan/CabManClient/login.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <QtWidgets/QWidget>
+#include "ui_login.h"
+
+class CabManClient : public QWidget
+{
+    Q_OBJECT
+
+public:
+    CabManClient(QWidget *parent = Q_NULLPTR);
+
+private:
+    Ui::CabManClientClass ui;
+};
