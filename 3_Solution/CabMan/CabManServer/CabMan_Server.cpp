c45e5deac86a7f751c779a2cefbd42aa16de6a12
FLORIN-GABRIEL CABAUA <79976336+CabauaFlorin@users.noreply.github.com>
2021-04-12 23:51:21 +0300

    [UPDATE] Server

--- 3_Solution/CabMan/CabManServer/CabMan_Server.cpp
+++ 3_Solution/CabMan/CabManServer/CabMan_Server.cpp
@@ -10,10 +10,10 @@ enum class CustomMsgTypes : uint32_t
 	ServerMessage,
 };
 
-class CustomServer : public olc::net::server_interface<CustomMsgTypes>
+class Server_app : public olc::net::server_interface<CustomMsgTypes>
 {
 public:
-	CustomServer(uint16_t nPort) : olc::net::server_interface<CustomMsgTypes>(nPort)
+	Server_app(uint16_t nPort) : olc::net::server_interface<CustomMsgTypes>(nPort)
 	{
 
 	}
@@ -65,7 +65,7 @@ protected:
 
 int main()
 {
-	CustomServer server(60000);
+	Server_app server(60000);
 	server.Start();
 
 	while (1)
